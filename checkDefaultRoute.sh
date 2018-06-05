23f06fb26822e056525403f2cfa575fdba932c5b
mselvaraj <mani_selvaraj@comcast.com>
2018-06-05 15:40:08 +0000

    DELIA-27970: Avoid multiple restart of systemd timesyncd
    Reason for change: The systemd timesyncd service has to be restarted only once as soon as the default route is available.
    The nlmon calls the subscribed script for default routes from all connected gateway and XB devices on the network.
    Test Procedure: Validate channel tune is working on rogers environment
    Risks: low
    
    Change-Id: I9131457d88c9843f359eaaebb4cbab896668a163
    Signed-off-by: mselvaraj <mani_selvaraj@comcast.com>

--- checkDefaultRoute.sh
+++ checkDefaultRoute.sh
@@ -1,5 +1,7 @@
 route -n
 ip -6 route
 echo "Input Arguments : $* "
-echo "Route is available" 
-touch /tmp/route_available
+echo "Route is available"
+if [ ! -f /tmp/route_available ];then
+    touch /tmp/route_available
+fi
